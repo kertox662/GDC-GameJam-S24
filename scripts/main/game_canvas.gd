b796117ed3ca14d1fb540e6cfb0eca0a3b7ab4cb
Misha Melnyk <as.k.github@melnyk.com>
2024-06-02 10:26:47 -0400

    scale fix

--- scripts/main/game_canvas.gd
+++ scripts/main/game_canvas.gd
@@ -3,8 +3,7 @@ extends CanvasLayer
 func _ready():
 	var music_tween = get_tree().create_tween()
 	music_tween.tween_property($GameMusic, "volume_db", -10, 2)
-
-
+	scale = Vector2(get_viewport().size) / Vector2(1152,648)
 
 func _on_game_music_finished():
 	$GameMusic.play()
